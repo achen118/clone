0a65fa4678e515de52556572a6066f5e127c8044
Alice Chen <alicechen118@gmail.com>
2017-07-17 15:30:51 -0700

    Fix rails routes and add static pages root

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,10 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'sessions/new'
+
+  root "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:new, :show, :create, :destroy]
+    resource :session, only: [:new, :create, :destroy]
   end
 
-  namespace :api do
-    get 'sessions/create'
-  end
-
-  namespace :api do
-    get 'sessions/destroy'
-  end
-
-  namespace :api do
-    get 'users/new'
-  end
-
-  namespace :api do
-    get 'users/show'
-  end
-
-  namespace :api do
-    get 'users/create'
-  end
-
-  namespace :api do
-    get 'users/destroy'
-  end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
