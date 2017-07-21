8272b60e50089ffdd0abcfa2ba51af8e923de1ba
Alice Chen <alicechen118@gmail.com>
2017-07-20 17:17:56 -0700

    Fix line height and text overflow in index items

--- frontend/components/notes/notes_index_item.jsx
+++ frontend/components/notes/notes_index_item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
+
 class NotesIndexItem extends React.Component {
 
   constructor(props) {
@@ -14,18 +15,35 @@ class NotesIndexItem extends React.Component {
 
   render() {
     const { note } = this.props;
+
     return (
+      <div>
         <article className="notes-index-item" onClick={ this.handleClick(note.id) }>
           <h3 className="notes-index-title">{ note.title }</h3>
-          <h4 className="notes-index-updated">Updated at</h4>
+          <h4 className="notes-index-updated">Yesterday</h4>
           <p className="notes-index-body">{ note.body }</p>
         </article>
+        <div className="bottom-border"></div>
+      </div>
     );
   }
 
 }
 
+function truncateText(text, maxLength) {
+  if (text.length > maxLength) {
+    text = text.substr(0,maxLength);
+    if (text[100] === " ")
+      return text;
+    else {
+      return text.substr(0, text.lastIndexOf(" "));
+    }
+  }
+}
+
 export default NotesIndexItem;
+
+
 
 // <article className="notes-index-item" onClick={ this.handleClick(note.id) }>
 //   <h3 className="note-title">{ note.title }</h3>
